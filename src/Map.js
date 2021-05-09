3da7e4ce0dcfbe15078e9a394347094bfc637eec
shackattackk <gaby_yusay2001@yahoo.com>
2021-05-09 21:39:48 +0800

    added marker logo

--- src/Map.js
+++ src/Map.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import './Map.css';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Circle } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+
+const defaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow
+});
+
+
+
 
 
 function Map() {
+
+  
+  
     return (
         <div className="map">
-            <MapContainer center={[10.6840, 122.9563]} zoom={9} scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    
-  </MapContainer>
+             <MapContainer center={[10.6840, 122.9563]} zoom={9} scrollWheelZoom={true}>
+                  <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
+
+                <Marker icon={defaultIcon} position={[10.683240064345533,122.95807465530876]}> </Marker> 
+                 
+                
+              </MapContainer>
   </div>
             
     )
